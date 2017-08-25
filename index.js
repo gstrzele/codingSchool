0bbe4fa14912402985e8435efc95a36e0d3acf88
Garrett Strzelecki <garrett.strzelecki@agilethought.com>
2017-08-25 15:28:08 -0400

    Calculate Fib array

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 function calculateFibonacci(sequenceLength) {
   // DECLARE AN ARRAY FOR HOLDING VALUES
+  var fibum = [];  
   // CREATE A LOOP TO CALCULATE THE FIBONACCI SEQUENCE
+  for(i=0;i<sequenceLength;i++){
+      if(i>1){     
+          fibum.push(fibum[i-2] + fibum[i-1]);  
+      } else{
+          fibum.push(i);
+      }
+  }
   // RETURN THE ARRAY THAT SHOULD NOW HOLD THE FIBONACCI SEQUENCE
+  return fibum;
 }
 
 function printArray(arrayToPrint) {
   // CREATE A LOOP TO DISPLAY THE CONTENTS OF arrayToPrint
+  //refactor string
+  var newstr = arrayToPrint.join(", ").toString();
+  
+  //print string
+  console.log(newstr);
 }
 
 var sequence = calculateFibonacci(10);
